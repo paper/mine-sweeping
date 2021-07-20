8334577824f060785972dcdd842970b3e836fd39
zhangbinjue <jue_paper@126.com>
2021-07-20 11:05:15 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:paper/mine-sweeping.git master:gh-pages
+git push -f git@github.com:paper/mine-sweeping.git main:gh-pages
 
 cd -
